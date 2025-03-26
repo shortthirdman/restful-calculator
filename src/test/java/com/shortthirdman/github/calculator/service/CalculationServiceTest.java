f91a1564ae2454b4e4498ac96b0499765d2ee22e
Swetank Mohanty <swetank.mohanty@outlook.com>
2025-03-26 16:11:43 +0530

    bugfix: Resolved loading of resource files for test cases

--- src/test/java/com/shortthirdman/github/calculator/service/CalculationServiceTest.java
+++ src/test/java/com/shortthirdman/github/calculator/service/CalculationServiceTest.java
@@ -4,34 +4,34 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shortthirdman.github.calculator.dto.Request;
 import com.shortthirdman.github.calculator.dto.Response;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CalculationServiceTest {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final CalculationService SERVICE = new CalculationService();
+    private static CalculationService service;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+    @BeforeAll
+    static void setUp() {
+        service = new CalculationService();
     }
 
     @TestFactory
     Stream<DynamicTest> testCalculateService() {
         int counter = 0;
-//        List<Stream<DynamicTest>> test = Collections.singletonList(generateTest("(+)", counter++, "request/1.json", "response/1.json"));
         List<Stream<DynamicTest>> test = Arrays.asList(
                 generateTest("(+)", counter++, "request/1.json", "response/1.json"),
                 generateTest("(+)", counter++, "request/2.json", "response/2.json"),
@@ -50,26 +50,32 @@ class CalculationServiceTest {
         return DynamicTest.dynamicTest(tag + " | Calculation test case #" + counter, () -> {
             var request = this.getRequest(requestPath);
             var expected = this.getResponse(responsePath);
-            var actual = SERVICE.calculate(request);
+            var actual = service.calculate(request);
             Assertions.assertEquals(expected.result(), actual.result());
         });
     }
 
     private Request getRequest(String path) {
         try {
-            byte[] content = Files.readAllBytes(Paths.get(CalculationServiceTest.class.getClassLoader().getResource(path).getPath()));
+            File file = ResourceUtils.getFile("classpath:" + path);
+
+            byte[] content = Files.readAllBytes(file.toPath());
+
             return OBJECT_MAPPER.readValue(new String(content, StandardCharsets.UTF_8), Request.class);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to read resource file", e);
         }
     }
 
     private Response getResponse(String path) {
         try {
-            byte[] content = Files.readAllBytes(Paths.get(CalculationServiceTest.class.getClassLoader().getResource(path).getPath()));
+            File file = ResourceUtils.getFile("classpath:" + path);
+
+            byte[] content = Files.readAllBytes(file.toPath());
+
             return OBJECT_MAPPER.readValue(new String(content, StandardCharsets.UTF_8), Response.class);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to read resource file", e);
         }
     }
 }
